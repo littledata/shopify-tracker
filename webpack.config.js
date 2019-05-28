7bf2e79bc0d481bdbf35e22142a2312747e139a2
Gabriel Bira <gabriel@littledata.io>
2019-05-28 16:16:04 +0300

    Use babel to transpile to basic js

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,18 @@ module.exports = {
 		gaTracker: './src/gaTracker.js',
 		segmentTracker: './src/segmentTracker.js',
 	},
+	module: {
+		rules: [{
+			test: /\.m?js$/,
+			exclude: /(node_modules|bower_components)/,
+			use: {
+				loader: 'babel-loader',
+				options: {
+					presets: ['@babel/preset-env']
+				}
+			}
+		}]
+	},
 	output: {
 		filename: '[name].js',
 		path: path.resolve(__dirname, 'dist'),
