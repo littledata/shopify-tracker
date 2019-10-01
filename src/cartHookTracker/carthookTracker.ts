dc46aab412078e601061a1524640e8ae2e3f68dc
Stefan Florescu <stefan@littledata.io>
2019-10-01 11:26:31 +0300

    take web property id from script source

--- src/cartHookTracker/carthookTracker.ts
+++ src/cartHookTracker/carthookTracker.ts
@@ -1,9 +1,8 @@
 /* eslint-env browser */
-import { getWebPropertyId, insertGtag, sendCartId, initGtag } from './helpers';
+import { getWebPropertyId, sendCartId, initGtag } from './helpers';
 
 (function() {
     const webPropertyID = getWebPropertyId();
-    insertGtag(webPropertyID);
     sendCartId();
     initGtag(webPropertyID);
 })();
