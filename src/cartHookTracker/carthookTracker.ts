b954119c85722311b3d5da926a2387ad98c2f2c0
Gabriel Bira <gabriel@littledata.io>
2019-10-03 10:44:41 +0300

    Fix carthook clientID for staging env

--- src/cartHookTracker/carthookTracker.ts
+++ src/cartHookTracker/carthookTracker.ts
@@ -2,7 +2,6 @@
 import { getWebPropertyId, sendCartId, initGtag, loadGtagScript } from './helpers';
 
 (function() {
-    console.log('location', JSON.stringify(location));
     const webPropertyPromise = getWebPropertyId();
 
     webPropertyPromise.then(webPropertyID => {
