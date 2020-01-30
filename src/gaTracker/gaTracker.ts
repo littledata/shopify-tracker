92e3bc830518ee25b6345434d68f91f6a974051f
Edward Upton <edward@littledata.io>
2020-01-30 16:04:29 +0200

    Bump script version

--- src/gaTracker/gaTracker.ts
+++ src/gaTracker/gaTracker.ts
@@ -4,7 +4,7 @@ import { initGtag, trackEvents, sendPageview } from './helpers';
 import { pageView, validateLittledataLayer, advertiseLD } from '../common/helpers';
 
 (function() {
-	window.LittledataScriptVersion = '8.0.4';
+	window.LittledataScriptVersion = '8.0.5';
 	validateLittledataLayer();
 	initGtag();
 	advertiseLD();
