5879cfd7bb4fb773bfd10d559b65a21fffb70d28
Edward Upton <edward@littledata.io>
2020-02-28 12:50:57 +0000

    Fix getCookie

--- src/common/getCookie.ts
+++ src/common/getCookie.ts
@@ -1,14 +1,14 @@
 export const getCookie = (name: string): string => {
 	if (document.cookie.length > 0) {
-		let cookieStart = document.cookie.indexOf(`${name}=`);
+		const cookieStart = document.cookie.indexOf(`${name}=`);
 		if (cookieStart !== -1) {
-			cookieStart = cookieStart + name.length + 1;
-			let cookieEnd = document.cookie.indexOf(';', cookieStart);
+			const valueStart = cookieStart + name.length + 1;
+			let cookieEnd = document.cookie.indexOf(';', valueStart);
 			if (cookieEnd === -1) {
 				cookieEnd = document.cookie.length;
 			}
-			const cookie = unescape(document.cookie.substring(cookieStart, cookieEnd));
-			if ((name = '_ga')) {
+			const cookie = unescape(document.cookie.substring(valueStart, cookieEnd));
+			if (name === '_ga') {
 				const match = cookie.match(/(\d{2,11})\.(\d{2,11})/g);
 				return match ? match[0] : '';
 			}
