147bef6e8cc4fc9f4bbcf82a4a6099389d4a896e
fstefan7 <stefan@littledata.io>
2020-07-22 14:22:11 +0300

    bump version

--- src/segmentTracker/segmentTracker.ts
+++ src/segmentTracker/segmentTracker.ts
@@ -5,7 +5,7 @@ import { pageView, validateLittledataLayer, advertiseLD, setClientID } from '../
 import { identifyCustomer, trackEvents, initSegment, callSegmentPage } from './helpers';
 
 (function() {
-	window.LittledataScriptVersion = '8.6';
+	window.LittledataScriptVersion = '8.7';
 	validateLittledataLayer();
 	initSegment();
 	advertiseLD();
