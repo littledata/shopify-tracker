31bdfc6b69a5e4e2df3cccf619fa9ac1ea4febaa
Codeship Build Script <build@codeship.com>
2020-09-07 17:26:22 +0000

    Build 6751db63a0953e84e9ea89b1946d2771cade4095

--- dist/gaTracker.js
+++ dist/gaTracker.js
@@ -130,6 +130,12 @@ function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
 
 function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(source, true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(source).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 
 
 
@@ -147,6 +153,15 @@ var initGtag = function initGtag() {
 
   gtag('js', new Date());
   gtag('config', LittledataLayer.webPropertyID, getConfig());
+
+  if (LittledataLayer.MPEndpoint) {
+    var length = LittledataLayer.MPEndpoint.length; // remove '/collect' from end, since it is added by gtag
+
+    var transport_url = LittledataLayer.MPEndpoint.slice(0, length - '/collect'.length);
+    gtag('config', 'CUSTOM-ENDPOINT', _objectSpread({}, getConfig(), {
+      transport_url: transport_url
+    }));
+  }
 };
 var postClientIdTimeout;
 var nextTimeout = 500; // half a second
