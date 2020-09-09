9c196451051802f7dc89805dd08682167fec6771
Mihai Cernat <mihaicernat@192-168-0-169.rdsnet.ro>
2020-09-09 09:59:14 +0300

    fix getProperties helper

--- src/segmentThankYouPageTracker/helpers.ts
+++ src/segmentThankYouPageTracker/helpers.ts
@@ -1,8 +1,7 @@
 export const getProperties = (scriptSrc: string): { webPropertyId: string; segmentProperty: string } => {
 	const startIndexGa = scriptSrc.indexOf('webPropertyId=');
-	const endIndexGa = scriptSrc.indexOf('&', startIndexGa);
-	const webPropertyId = startIndexGa && scriptSrc.substring(startIndexGa + 14, endIndexGa);
-	const startIndexSegment = scriptSrc.indexOf('segmentProperty=', endIndexGa);
-	const segmentProperty = startIndexSegment && scriptSrc.substring(startIndexSegment);
+	const webPropertyId = startIndexGa && scriptSrc.substring(startIndexGa + 14);
+	const startIndexSegment = scriptSrc.indexOf('segmentProperty=');
+	const segmentProperty = startIndexSegment && scriptSrc.substring(startIndexSegment + 16);
 	return { webPropertyId, segmentProperty };
 };
