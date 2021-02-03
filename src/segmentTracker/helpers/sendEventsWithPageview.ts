0762d10387f5a8a6af91e8d0853aa585858ade09
Edward Upton <edward@littledata.io>
2021-02-03 16:12:04 +0000

    Add more events

---
+++ src/segmentTracker/helpers/sendEventsWithPageview.ts
@@ -0,0 +1,15 @@
+import { trackEvent } from '../combinedHelpers';
+
+export const sendEventsWithPageview = (pathname: string) => {
+	if (pathname === '/cart') {
+		trackEvent('Cart Viewed', {});
+	}
+	if (pathname === '/account/register') {
+		trackEvent('Registration Viewed', {});
+	}
+	if (pathname === '/search') {
+		trackEvent('Products Searched', {
+			query: document.location.search.replace('?q=', '').replace('+', ' '),
+		});
+	}
+};
